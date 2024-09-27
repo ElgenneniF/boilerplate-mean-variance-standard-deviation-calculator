d7ecf33bdf92d0c3ebad44c3dc8786a8a722dca2
Felipe Elgenneni <Felipe.Elgenneni@sou.inteli.edu.br>
2024-09-27 22:18:01 +0000

    pff funciona

--- mean_var_std.py
+++ mean_var_std.py
@@ -2,7 +2,21 @@ import numpy as np
 
 def calculate(list):
 
-
-
+    # ve se tem 9 elementos
+    if len(input_list) != 9:
+        raise ValueError("List must contain nine numbers.")
+    
+    # faz um array 3x3
+    matrix = np.array(input_list).reshape(3, 3)
+    
+    # Calcula as estatisticas
+    calculations = {
+        'mean': [list(matrix.mean(axis=0)), list(matrix.mean(axis=1)), matrix.mean()],
+        'variance': [list(matrix.var(axis=0)), list(matrix.var(axis=1)), matrix.var()],
+        'standard deviation': [list(matrix.std(axis=0)), list(matrix.std(axis=1)), matrix.std()],
+        'max': [list(matrix.max(axis=0)), list(matrix.max(axis=1)), matrix.max()],
+        'min': [list(matrix.min(axis=0)), list(matrix.min(axis=1)), matrix.min()],
+        'sum': [list(matrix.sum(axis=0)), list(matrix.sum(axis=1)), matrix.sum()]
+    }
 
     return calculations
